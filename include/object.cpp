06ea505b92ca15a9f8d5131b2e2db3af726b4e99
Jonathan Stancik <jonathan.e.stancik@gmail.com>
2022-01-06 21:29:54 -0500

    Preparing for obj file loading

--- include/object.cpp
+++ include/object.cpp
@@ -10,3 +10,11 @@
 Object::Object(int dChar, int iChar,float data[],unsigned int index[]):vb(dChar,data),ib(iChar,index){
 	
 }
+
+Object::Object(std::string filePath):vb(0,{}),ib(0,{}){
+	
+}
+
+void Object::addModel(std::string filePath){
+	
+}
--- include/object.hpp
+++ include/object.hpp
@@ -8,6 +8,14 @@ class Object{
 		VertexBuffer vb;
 		IndexBuffer ib;
 		Object(int dataCharSize, int indexCharSize,float data[],unsigned int index[]);
+		/**
+		 * \param filePath path of .obj file to generate Object from
+		*/
+		Object(std::string filePath);
+		/**
+		 * \param filePath path of .obj file to add from
+		*/
+		void addModel(std::string filePath);
 	private:
 
 };
